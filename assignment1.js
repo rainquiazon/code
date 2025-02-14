5eee22ee47051cf7bbc1220ba1200417476645eb
rainquiazon <rainquuanzon@gmail.com>
2025-02-14 16:13:16 +0800

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,47 @@
+// CS2A_RQuianzon_C1-A1.js
+
+// Student Information
+const student1 = {
+    firstName: "Rain",
+    middleName: "L.",
+    lastName: "Quianzon",
+    birthdate: "October 3, 2006",
+    birthplace: "Apatot, San Esteban, Ilocos Sur",
+    address: "Apatot, San Esteban, Ilocos Sur",
+    course: "Bachelor of Science in Computer Science",
+    dreamJob: "Software Engineer"
+};
+
+const student2 = {
+    firstName: "jasper",
+    middleName: "vergara",
+    lastName: " barga",
+    birthdate: "november 11, 2005",
+    birthplace: "Apatot, San Esteban, Ilocos Sur ,  ",
+    address: "Apatot, San Esteban, Ilocos Sur",
+    course: "Bachelor of Science in Information Technology",
+    dreamJob: "Cybersecurity Analyst"
+};
+
+const student3 = {
+    firstName: "Jose",
+    middleName: "Sabugo",
+    lastName: "Ricafrente",
+    birthdate: "March 10, 2006",
+    birthplace: "Balibago, Angeles City, Pampanga, Philippines 2009",
+    address: "Apatot, San Esteban, Ilocos Sur",
+    course: "Bachelor of Science in Computer Engineering",
+    dreamJob: "AI Developer"
+};
+
+// Function to format and print student details
+function printStudentInfo(student) {
+    console.log(`[${student.firstName.toUpperCase()} ${student.middleName.toUpperCase()} ${student.lastName.toUpperCase()}] was born [${student.birthdate}] at [${student.birthplace.toUpperCase()}], and currently living at [${student.address.toUpperCase()}].`);
+    console.log(`[${student.firstName.toLowerCase()} ${student.middleName.toLowerCase()} ${student.lastName.toLowerCase()}] is taking up [${student.course.toLowerCase()}] and dreams to be [${student.dreamJob}] after graduation.`);
+    console.log("\n");
+}
+
+// Print all student information
+printStudentInfo(student1);
+printStudentInfo(student2);
+printStudentInfo(student3);
