2b35254b1a3b15fd6c673df07175c0e65c5a786b
rainquiazon <rainquuanzon@gmail.com>
2025-04-02 18:06:48 +0800

    Update array activity.js

---
+++ object activity/array activity.js
@@ -0,0 +1,17 @@
+// Make an array with favorite singers
+let favSingers = ["arthur nery", "alek olsen", "clairo"];
+
+// Show the first singer in the list
+console.log(favSingers[0]);
+
+// Make an array with favorite numbers
+let favNumbers = [7, 14, 21, 28];
+
+// Make an array with different types of data
+let mixedArr = ["string", ["otherarray"], 123, true];
+
+// Show each item in mixedArr
+console.log(mixedArr[0]); // Shows "string"
+console.log(mixedArr[1]); // Shows ["otherarray"]
+console.log(mixedArr[2]); // Shows 123
+console.log(mixedArr[3]); // Shows true
