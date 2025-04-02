d4979c973db8764ba7b9d4ea2d729a78c3cf0901
rainquiazon <rainquuanzon@gmail.com>
2025-04-02 18:07:51 +0800

    Update object activity.js

---
+++ object activity/object activity.js
@@ -0,0 +1,17 @@
+// Make an object with car details
+let car = {
+    type: "montero",
+    model: "2024",
+    color: "Black"
+};
+
+// Show what type of data 'car' is
+console.log(typeof car);
+
+// Change car type to "Toyota"
+car.type = "mitsubishi";
+console.log(car);
+
+// Add a new detail: the car has 4 wheels
+car.wheels = 4;
+console.log(car);
